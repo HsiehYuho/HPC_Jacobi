41cf7de9ba9396a69a38024637146bdda9e7387d
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2019-04-05 09:30:46 -0400

    Implement transpose_bcast_vector function and move tests into functions

--- mpi_jacobi.cpp
+++ mpi_jacobi.cpp
@@ -154,7 +154,7 @@ void distribute_matrix(const int n, double* input_matrix, double** local_matrix,
                     }
                 }
 
-                // send out the data to correpsponding destination
+                // Send out the data to correpsponding destination
                 int dest_coords[2] = {row, col};
                 int dest_rank = 0;
                 MPI_Cart_rank(comm, dest_coords, &dest_rank);
@@ -191,14 +191,77 @@ void distribute_matrix(const int n, double* input_matrix, double** local_matrix,
 
 void transpose_bcast_vector(const int n, double* col_vector, double* row_vector, MPI_Comm comm)
 {
-    // TODO
+    int myrank;
+    MPI_Comm_rank(comm, &myrank);
+
+    int dims[2];
+    int periods[2];
+    int coords[2];
+    MPI_Cart_get(comm, 2, dims, periods, coords);
     
+    // (x,0) processor has col_vector and send it to (i,i) cell
+    if(coords[1] == 0){
+        int dest_rank = 0;
+        int dest_coords[2] = {coords[0],coords[0]};
+        MPI_Cart_rank(comm, dest_coords, &dest_rank);
+        
+        int vec_elem_num = get_cell_elem_num(coords[0], dims[0], n);
+        MPI_Send(col_vector, vec_elem_num, MPI_DOUBLE, dest_rank, 222, comm);
+    }
+
+    // (x,x) processor expects to receive data from (x,0) then broadcast to sub-communicators
+    int vec_elem_num = get_cell_elem_num(coords[0], dims[0], n);
+
+    if(coords[0] == coords[1]){
+        MPI_Status stat; 
+
+        int source_rank = 0;
+        int source_coords[2] = {coords[0],0};
+        MPI_Cart_rank(comm, source_coords, &source_rank);
+        MPI_Recv(row_vector, vec_elem_num, MPI_DOUBLE, source_rank, 222, comm, &stat);
+    }
+
+    // May not be necessary, but just in case
+    MPI_Barrier(comm);
+
+    // Set the keep each row but drop column
+    int remains_dim[2] = {1,0}; 
+
+    // Create sub communicators
+    MPI_Comm col_comm;
+    MPI_Cart_sub(comm, remains_dim, &col_comm);
+
+    // Broadcast vec to same column cells
+    int root_rank = 0;
+    int root_coords[1] = {coords[1]};
+    MPI_Cart_rank(col_comm, root_coords, &root_rank);
+    MPI_Bcast(row_vector, vec_elem_num, MPI_DOUBLE, root_rank, col_comm);
+
+    MPI_Comm_free(&col_comm);
 }
 
 
 void distributed_matrix_vector_mult(const int n, double* local_A, double* local_x, double* local_y, MPI_Comm comm)
 {
     // TODO
+    int myrank;
+    MPI_Comm_rank(comm, &myrank);
+
+    int dims[2];
+    int periods[2];
+    int coords[2];
+    MPI_Cart_get(comm, 2, dims, periods, coords);
+
+    int vec_elem_num = get_cell_elem_num(coords[0], dims[0], n);
+    double *row_vec = new double[vec_elem_num];
+    // First transpose and bcast vector to get the local vector
+    // only (x,0) processor has local_x
+    transpose_bcast_vector(n, local_x, row_vec, comm);
+    printf("(%d, %d): \n", coords[0], coords[1]);
+    for(int i = 0; i < vec_elem_num; i++)
+        printf("%.2f, ", row_vec[i]);
+    printf("\n");
+
 }
 
 // Solves Ax = b using the iterative jacobi method
@@ -208,6 +271,56 @@ void distributed_jacobi(const int n, double* local_A, double* local_b, double* l
     // TODO
 }
 
+void print_sent_matrix(double* A, int n){
+    printf("Print input matrix: \n");
+    for(int i = 0; i < n*n; i++){
+        printf("%.2f, ", A[i]);
+    }   
+    printf("\n");
+}
+
+// cmd: mpirun -np 9 --oversubscribe  ./jacobi -n 5
+void test_distribute_matrix(double* local_A, int n, MPI_Comm comm){
+    if(local_A){
+        int dims[2];
+        int periods[2];
+        int coords[2];
+        MPI_Cart_get(comm, 2, dims, periods, coords);
+
+        int row_elem_num = get_cell_elem_num(coords[0], dims[0], n);
+        int col_elem_num = get_cell_elem_num(coords[1], dims[1], n);
+
+        int elem_num = row_elem_num * col_elem_num;
+        printf("(%d, %d) matrix elem_num %d \n",coords[0], coords[1], elem_num);
+        for(int i = 0; i < elem_num; i++)
+            printf("%.2f, ", local_A[i]);
+        printf("\n");
+    }
+}
+
+void test_distribute_vector(double* local_b, int n, MPI_Comm comm){
+    if(local_b){
+        int dims[2];
+        int periods[2];
+        int coords[2];
+        MPI_Cart_get(comm, 2, dims, periods, coords);
+
+        int elem_num = get_cell_elem_num(coords[0], dims[0], n);
+        printf("(%d, %d) vector elem_num %d \n",coords[0], coords[1], elem_num);
+    }
+}
+void test_gather_vector(double* x, int n, MPI_Comm comm){
+    if(x){
+        int myrank;
+        MPI_Comm_rank(comm, &myrank);
+        if(myrank == 0){
+            for(int i = 0; i < n; i++){
+                printf("%.2f, ", x[i]);
+            }        
+            printf("\n");            
+        }
+    }
+}
 
 // wraps the distributed matrix vector multiplication
 void mpi_matrix_vector_mult(const int n, double* A,
@@ -218,6 +331,16 @@ void mpi_matrix_vector_mult(const int n, double* A,
     double* local_x = NULL;
     distribute_matrix(n, &A[0], &local_A, comm);
     distribute_vector(n, &x[0], &local_x, comm);
+
+    // /*For test*/
+    // int coords00[2] = {0,0};
+    // int myrank = 0, rank00 = 0;
+    // MPI_Cart_rank(comm, coords00, &rank00);
+    // MPI_Comm_rank(comm, &myrank);
+
+    // if(myrank == rank00){
+    //     print_sent_matrix(A, n);
+    // }
 
     // allocate local result space
     double* local_y = new double[block_decompose_by_dim(n, comm, 0)];
@@ -237,65 +360,15 @@ void mpi_jacobi(const int n, double* A, double* b, double* x, MPI_Comm comm,
     distribute_matrix(n, &A[0], &local_A, comm);
     distribute_vector(n, &b[0], &local_b, comm);
     
-    /*For test*/
-    int coords00[2] = {0,0};
-    int myrank = 0, rank00 = 0;
-    MPI_Cart_rank(comm, coords00, &rank00);
-    MPI_Comm_rank(comm, &myrank);
-
-    if(myrank == rank00){
-        printf("Print input matrix: \n");
-        for(int i = 0; i < n*n; i++){
-            printf("%.2f, ", A[i]);
-        }   
-        printf("\n");
-    }
-
-    // test distribute_matrix
-    // cmd: mpirun -np 9 --oversubscribe  ./jacobi -n 5
-    if(local_A){
-        int dims[2];
-        int periods[2];
-        int coords[2];
-        MPI_Cart_get(comm, 2, dims, periods, coords);
-
-        int row_elem_num = get_cell_elem_num(coords[0], dims[0], n);
-        int col_elem_num = get_cell_elem_num(coords[1], dims[1], n);
-
-        int elem_num = row_elem_num * col_elem_num;
-        printf("(%d, %d) matrix elem_num %d \n",coords[0], coords[1], elem_num);
-        for(int i = 0; i < elem_num; i++)
-            printf("%.2f, ", local_A[i]);
-        printf("\n");
-    }
-
-    // test distribute_vector
-    if(local_b){
-        int dims[2];
-        int periods[2];
-        int coords[2];
-        MPI_Cart_get(comm, 2, dims, periods, coords);
-
-        int elem_num = get_cell_elem_num(coords[0], dims[0], n);
-        printf("(%d, %d) vector elem_num %d \n",coords[0], coords[1], elem_num);
-    }
+    /*** TEMP ***/
+    double* local_y = new double[block_decompose_by_dim(n, comm, 0)];
+    distributed_matrix_vector_mult(n, local_A, local_b, local_y, comm);
+    /*** TEMP ***/
 
     // allocate local result space
-    double* local_x = new double[block_decompose_by_dim(n, comm, 0)];
-    distributed_jacobi(n, local_A, local_b, local_x, comm, max_iter, l2_termination);
+    // double* local_x = new double[block_decompose_by_dim(n, comm, 0)];
+    // distributed_jacobi(n, local_A, local_b, local_x, comm, max_iter, l2_termination);
 
     // gather results back to rank 0
-    gather_vector(n, local_x, x, comm);
-
-    // test gather_vector
-    // if(x){
-    //     int myrank;
-    //     MPI_Comm_rank(comm, &myrank);
-    //     if(myrank == 0){
-    //         for(int i = 0; i < n; i++){
-    //             printf("%.2f, ", x[i]);
-    //         }        
-    //         printf("\n");            
-    //     }
-    // }
+    // gather_vector(n, local_x, x, comm);
 }
